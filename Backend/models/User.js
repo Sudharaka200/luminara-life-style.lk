d3ff42ac9bd057777592693dbe1d3f99b8409ab2
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-09-03 15:27:35 +0530

    user Auth API done

---
+++ Backend/models/User.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+//user database
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 3,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+    }
+}, {
+    timestamps: true, // Automatically adds createdAt and updatedAt fields
+}
+
+);
+
+export default mongoose.model("UserData", userSchema);
