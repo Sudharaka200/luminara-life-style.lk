6e2b6b8f7888cd7cbfb1805d7384b2cd3da21b11
Sudharaka Pathiraja <114396209+Sudharaka200@users.noreply.github.com>
2025-09-19 13:08:22 +0530

    update Property Home page

--- src/app/PropertyHome/page.tsx
+++ src/app/PropertyHome/page.tsx
@@ -2,8 +2,11 @@
 import CoverImg from '@/../public/images/Rectangle 109.png'
 import Image from 'next/image'
 import ProfileImg from '@/../public/images/Rectangle 183.png'
-import { MapPin, Bed, Bath } from "lucide-react"; 
-
+import { MapPin, Bed, Bath } from "lucide-react";
+import CarosalHome from '../components/carosalHome';
+import GridImg1 from '@/../public/images/Rectangle 193.png'
+import GridImg2 from '@/../public/images/Rectangle 195.png'
+import Propertycard from '../components/card'
 
 function page() {
     return (
@@ -24,7 +27,104 @@ function page() {
                     <MapPin size={16} className="mr-1" />
                     <p>Gampaha</p>
                 </div>
+                <CarosalHome />
             </div>
+
+            <div className='container mx-auto'>
+                <div className='flex'>
+                    <h1>27,200,000 </h1>
+                    <h1> LKR</h1>
+                </div>
+                <p>PER UNIT UPWARDS</p>
+                <p>Welcome to Elemint Suites by Prime Lands an exquisite housing project nestled in the heart of Gampaha. With a captivating scenic lake view. Our project offers a collection of six types of individual houses ranging from single-story to two-story designed to cater to your every need. Choose from our 2 or 3-bedroom houses each thoughtfully crafted to provide a comfortable and modern living experience, with sizes ranging from 855 square feet to 1974 square feet.Welcome to Elemint Suites by Prime Lands an exquisite housing project nestled in the heart of Gampaha. With a captivating scenic lake view. Our project offers a collection of six types of individual houses ranging from single-story to two-story designed to cater to your every need. Choose from our 2 or 3-bedroom houses each thoughtfully crafted to provide a comfortable and modern living experience, with sizes ranging from 855 square feet to 1974 square feet.Welcome to Elemint Suites by Prime Lands an exquisite housing project nestled in the heart of Gampaha. With a captivating scenic lake view. Our project offers a collection of six types of individual houses ranging from single-story to two-story designed to cater to your every need. Choose from our 2 or 3-bedroom houses each thoughtfully crafted to provide a comfortable and modern living experience, with sizes ranging from 855 square feet to 1974 square feet.</p>
+                <ul>
+                    <li>Scenic Lake View</li>
+                    <li>Scenic Lake View</li>
+                    <li>Scenic Lake View</li>
+                    <li>Scenic Lake View</li>
+                    <li>Scenic Lake View</li>
+                    <li>Scenic Lake View</li>
+                    <li>Scenic Lake View</li>
+                </ul>
+
+                <button>Schedule Free Consultation</button>
+            </div>
+
+            <div className='container mx-auto'>
+                <div className="aspect-w-16 aspect-h-9">
+                    <iframe
+                        src="https://www.youtube.com/embed/VIDEO_ID"
+                        title="YouTube video"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        className="w-full h-150"
+                    ></iframe>
+                </div>
+
+                <div>
+                    <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+                        <div>
+                            <Image src={GridImg1} alt='GridImg' />
+                        </div>
+                        <div>
+                            <Image src={GridImg2} alt='' />
+                        </div>
+                    </div>
+
+                    <div className="w-full h-[400px]">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d63321.52423162401!2d79.8612432!3d6.9270786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ae2595d01f52dfb%3A0x8a82a5f5ff2fbea!2sColombo!5e0!3m2!1sen!2slk!4v1694836922337!5m2!1sen!2slk"
+                            width="100%"
+                            height="100%"
+                            style={{ border: 0 }}
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                        ></iframe>
+                    </div>
+                </div>
+            </div>
+
+            <div className='container mx-auto'>
+                <h1>Recomomned</h1>
+                <div className='grid grid-cols-1 md:grid-cols-4 gap-5'>
+          <Propertycard
+            title="EXPRESS FIVE - MATTEGODA"
+            location="Borella"
+            price="39,500,000"
+            image="/images/Rectangle 103.png"
+            beds={3}
+            baths={1}
+            link="/property/express-five"
+          />
+          <Propertycard
+            title="EXPRESS FIVE - MATTEGODA"
+            location="Borella"
+            price="39,500,000"
+            image="/images/Rectangle 103.png"
+            beds={3}
+            baths={1}
+            link="/property/express-five"
+          />
+          <Propertycard
+            title="EXPRESS FIVE - MATTEGODA"
+            location="Borella"
+            price="39,500,000"
+            image="/images/Rectangle 103.png"
+            beds={3}
+            baths={1}
+            link="/property/express-five"
+          />
+          <Propertycard
+            title="EXPRESS FIVE - MATTEGODA"
+            location="Borella"
+            price="39,500,000"
+            image="/images/Rectangle 103.png"
+            beds={3}
+            baths={1}
+            link="/property/express-five"
+          />
+        </div>
+            </div>  
         </>
     )
 }
